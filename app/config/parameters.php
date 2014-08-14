dd53626d2a27143ae338e565984ba0202d9f7944
Pablo Martelletti <pmartelletti@gmail.com>
2014-08-14 15:46:36 -0300

    Edit installation composer.

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -8,3 +8,12 @@ if (isset($_ENV['DB_HOST'])) {
     $container->setParameter('database_password', $_ENV['DB_PASS']);
     $container->setParameter('database_port', $_ENV['DB_PORT']);
 }
+
+if (isset($_ENV['MANDRILL_SUBACCOUNT'])) {
+    $container->setParameter('mandrill_subaccount', $_ENV['MANDRILL_SUBACCOUNT']);
+}
+
+if (isset($_ENV['SIESC_INSTITUCION'])) {
+    $container->setParameter('institucion', $_ENV['SIESC_INSTITUCION']);
+    $container->setParameter('project_code', $_ENV['SIESC_INSTITUCION_CODE']);
+}
